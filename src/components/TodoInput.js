a16446df005cec67e30c4daba84612f471c4119b
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2019-09-08 21:40:30 +0300

    add form

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import { classBody } from '@babel/types';
 
 export default class TodoInput extends Component {
     render() {
+        const {item, handleChange, handleSubmit, editI} = this.props;
         return (
-            <h1>
-                hello from todo input
-            </h1>
+            <div className="card card-body my-3">
+                <form onSubmit={handleSubmit}>
+                    <div className="input-group">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text bg-primary text-white">
+                                <i className="fa fa-book" />
+                            </div>
+                        </div>
+                        <input type="text" 
+                        className="form-control text-capitalize"
+                        placeholder="add todo item"
+                        value={item}
+                        onChange={handleChange}
+                        />
+                    </div> 
+                    <button type="submit" className="btn btn-block btn-primary mt-3 
+                    text-uppercase">
+                        add item
+                    </button>   
+                </form>
+            </div>
         )
     }
 }
