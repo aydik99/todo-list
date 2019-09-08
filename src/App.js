0e9980f5062817d35cf770685a0b94185e1752bf
Алиев Айдемир Камильевич <aydik@MacBook-Air-Aliev.local>
2019-09-08 21:12:43 +0300

    app component structer

--- src/App.js
+++ src/App.js
@@ -6,16 +6,57 @@ import TodoList from './components/TodoList';
 //a
 
 class App extends Component {
+  state = {
+    items: [{id:1, title:"wale up"}, {id:2, title:"make breakfast"}],
+    id: uuid(),
+    item: '',
+    editItem: false
+  };
+
+  handleChange = (e) => {
+
+  }
+
+  handleSubmit = (e) => {
+
+  }
+
+  clearList = () => {
+
+  }
+
+  handleDelete = (id) => {
+
+  }
+
+  handleEdit = (id) => {
+    
+  }
+
   render() {
     return (  
-      <div>
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
-        </div>
-      </div>
-      </div>
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">
+              todo input
+            </h3>
+            <TodoInput 
+            item={this.state.item}
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+            editItem={this.state.editItem}
+            />
+             <TodoList 
+              items={this.state.items}
+              clearList={this.clearList}
+              handleDelete={this.handleDelete}
+              handleEdit={this.handleEdit}
+             />
+            </div>
+            </div>
+            </div>
+          
     )
   }
 }
